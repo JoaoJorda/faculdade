f4029dd056bfc056f818c0277ddc7d056d7781a8
JoaoJorda <joaojordao2014@gmail.com>
2024-04-11 00:15:35 -0300

    faculade

---
+++ login_faculdade/aluno.php
@@ -0,0 +1,44 @@
+<?php include"menu.php"; ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/aluno.css">
+    <title>Área do aluno</title>
+</head>
+<body>
+    <!-- <div class="container">
+        <h2>Área do Aluno</h2>
+        <div class="card">
+            <img src="img/biblioteca.jpg" alt="foto1">
+            <h4>Biblioteca</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates dolore alias, repellat quam repellendus dignissimos nostrum sunt ab, debitis laudantium neque quae porro. Veritatis unde sequi quas tempore commodi illum?</p>
+        </div>
+        <div class="card">
+            <img src="img/formatura.jpg" alt="foto2">
+            <h4>Formatura</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates dolore alias, repellat quam repellendus dignissimos nostrum sunt ab, debitis laudantium neque quae porro. Veritatis unde sequi quas tempore commodi illum?</p>
+        </div>
+        <button>Sair</button>
+    </div> -->
+    <div class="container">
+        <div class="texto_principal">
+        <h1>Área do Aluno</h1>
+            </div>
+                <div class="container_card">
+                    <div class="card">
+                        <img src="img/formatura.jpg" alt="imagem1">
+                        <h4>Formatura</h4>
+                        <p class="texto_card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore quaerat sequi ipsa sapiente doloremque quod eligendi aperiam sint, dolores aliquid. Omnis saepe alias dolore pariatur, veritatis culpa quis nisi non!</p>
+                    </div>
+                    <div class="card">
+                        <img src="img/biblioteca.jpg" alt="imagem2">
+                        <h4>Biblioteca</h4>
+                        <p class="texto_card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore quaerat sequi ipsa sapiente doloremque quod eligendi aperiam sint, dolores aliquid. Omnis saepe alias dolore pariatur, veritatis culpa quis nisi non!</p>
+                </div>
+            </div>
+        <button>Sair</button>
+    </div>
+</body>
+</html>
---
+++ login_faculdade/dashboard.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    if(empty($_SESSION)){
+        print"<script>location.href='index.php';</scrpt>";
+    }
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de login</title>
+</head>
+<body>
+    
+</body>
+</html>
